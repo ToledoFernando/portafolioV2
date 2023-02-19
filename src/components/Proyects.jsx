af6a46b60f978fe96b6edd3ac157afad10835f71
toledofernando <toledof764@gmail.com>
2023-02-19 15:36:12 -0300

    add data

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,7 +1,13 @@
 import style from "@/styles/Proyects.module.scss";
+import data from "./proyects.json";
 
 function Proyects() {
-  return <div>Proyects</div>;
+  console.log(data);
+  return (
+    <div className={style.proyects}>
+      <h1>Hello world proyects</h1>
+    </div>
+  );
 }
 
 export default Proyects;
--- src/components/TechSkill.jsx
+++ src/components/TechSkill.jsx
@@ -8,40 +8,43 @@ function TechSkill() {
         <h2>Front End</h2>
         <p>Conocimientos en tecnologías de desarrollo Front end.</p>
         <ul>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>SASS</li>
-          <li>Javascript</li>
-          <li>Typescript</li>
-          <li>ReactJS</li>
-          <li>Redux</li>
+          <li title="HTML5">HTML5</li>
+          <li title="CSS3">CSS3</li>
+          <li title="SASS">SASS</li>
+          <li title="Javascript">Javascript</li>
+          <li title="Typescript">Typescript</li>
+          <li title="ReactJS">ReactJS</li>
+          <li title="Redux">Redux</li>
         </ul>
+        <span className={style.dv}></span>
       </div>
       <div>
         <img src="/back.svg" alt="" />
         <h2>Back End</h2>
         <p>Conocimientos en tecnologias del lado del servidor</p>
         <ul>
-          <li>NodeJS</li>
-          <li>Express</li>
-          <li>Typescript</li>
-          <li>PostgreSQL</li>
-          <li>Sequelize</li>
-          <li>MongoDB</li>
-          <li>Mongoose</li>
+          <li title="NodeJS">NodeJS</li>
+          <li title="Express">Express</li>
+          <li title="Typescript">Typescript</li>
+          <li title="PostgreSQL">PostgreSQL</li>
+          <li title="Sequelize">Sequelize</li>
+          <li title="MongoDB">MongoDB</li>
+          <li title="Mongoose">Mongoose</li>
         </ul>
+        <span className={style.dv}></span>
       </div>
       <div>
         <img src="/h.svg" alt="" />
         <h2>otras Herramientas</h2>
         <p>Herramientas que suelo utilizar al desarrollar</p>
         <ul>
-          <li>GitHub</li>
-          <li>Git</li>
-          <li>Visual Studio Code</li>
-          <li>Canvas</li>
+          <li title="GitHub">GitHub</li>
+          <li title="Git">Git</li>
+          <li title="Visual">Visual Studio Code</li>
+          <li title="Canvas">Canvas</li>
         </ul>
       </div>
+      <span className={style.span}></span>
     </div>
   );
 }
